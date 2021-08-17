7f7d3bade8c2d3322ed1d74a2b1922135a2021d6
WarriorsSami <sbarbutdica@gmail.com>
2021-08-17 20:09:34 +0300

    created foundation project for rust iterator

---
+++ Iterator/Rust/target/debug/Iterator-DP.d
@@ -0,0 +1,1 @@
+/media/sami/Dev\ Space/Design-Patterns-Catalog/Iterator/Rust/target/debug/Iterator-DP: /media/sami/Dev\ Space/Design-Patterns-Catalog/Iterator/Rust/src/main.rs
---
+++ Iterator/Rust/target/debug/deps/Iterator_DP-3ef0cfb3f14cd889.d
@@ -0,0 +1,5 @@
+/media/sami/Dev Space/Design-Patterns-Catalog/Iterator/Rust/target/debug/deps/Iterator_DP-3ef0cfb3f14cd889: src/main.rs
+
+/media/sami/Dev Space/Design-Patterns-Catalog/Iterator/Rust/target/debug/deps/Iterator_DP-3ef0cfb3f14cd889.d: src/main.rs
+
+src/main.rs:
